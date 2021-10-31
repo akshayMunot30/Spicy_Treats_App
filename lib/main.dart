02136dff7fef0c11694180996ef5d5e1ca15789f
akshaymunot30 <akshaymunot30@gmail.com>
2021-11-01 01:37:07 +0530

    Created the first screen for the app.
    - Added a grid view, which contain different categories.
    - Each category is basically a container to which I have added
    gradient effect.
    - Added the fonts files, so as to use them in the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './categories_screen.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -10,30 +12,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Spicy Treats',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'RobtoCondensed',
+              ),
+            ),
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Spicy Treats"),
-      ),
-      body: Center(
-        child: Text('Navigation Time!'),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: CategoriesScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
