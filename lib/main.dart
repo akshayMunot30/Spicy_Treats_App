013111f296d817692d085be983b9f2be9bd30a88
akshaymunot30 <akshaymunot30@gmail.com>
2021-11-01 00:21:26 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spicy Treats',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Spicy Treats"),
+      ),
+      body: Center(
+        child: Text('Navigation Time!'),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
