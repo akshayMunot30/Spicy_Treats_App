6c199a7f741b4c8348a97fa4f7a2a9ef9994f6ed
akshaymunot30 <akshaymunot30@gmail.com>
2022-01-14 18:28:40 +0530

    Added the list of Recipes
    - Added cards inside every category
    - Used Stack and Positioned widgets to display name of Recipe
    - Also added the Time, Complexity and Affordability of Recipe

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import './categories_screen.dart';
-import 'category_meals_screen.dart';
+import '../screens/categories_screen.dart';
+import '../screens/category_meals_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -48,8 +48,11 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: CategoriesScreen(),
-      routes: {'/category-meals': (ctx) => CategoryMealsScreen()},
+      // home: CategoriesScreen(),
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
       debugShowCheckedModeBanner: false,
     );
   }
